722b834d22285c8929226606b839b9eb661ff1c9
samin <samin@student.42seoul.kr>
2023-02-23 13:15:42 +0900

    fix templateMethodPattern

--- templateMethodPattern/Car.java
+++ templateMethodPattern/Car.java
@@ -1,18 +1,20 @@
 package samin.templateMethodPattern;
 
 public abstract class Car {
-	abstract public void startCar();
-
-	abstract public void drive();
-
-	abstract public void stop();
-
-	abstract public void turnOff();
-
-	final void run() {
-		startCar();
-		drive();
-		turnOff();
-		stop();
-	};
+    abstract public void drive();
+    abstract public void turn();
+    abstract public void stop();
+    private void startCar() {
+        System.out.println("시동을 켭니다.");
+    }
+    private void turnOff() {
+        System.out.println("시동을 끕니다.");
+    }
+    final void run() {
+        startCar();
+        drive();
+        turn();
+        stop();
+        turnOff();
+    };
 }
